ec95bd3917098bd9f0a169ae23a1371995e66644
w-jins <jineen@naver.com>
2025-03-27 00:59:26 +0900

    React Mission 1 Async/Await

--- React-Mission/src/components/AsyncFetch.jsx
+++ React-Mission/src/components/AsyncFetch.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axiosAPI from "../api/axios";
+import request from "../api/requests";
 
 export default function AsyncFetch() {
-  return <div>hi</div>;
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    const res = async () => {
+      const data = await axiosAPI.get(request.fetchNetflixOriginals);
+      setMovie(data.data.results);
+    };
+    res();
+  }, []);
+  return (
+    <div>
+      <div className='grid grid-cols-3 gap-4'>
+        {movie.map((item) => (
+          <div key={item.id} className='p-4 border-2 border-gray-200'>
+            <p className='text-center text-xs mb-2'>{item.name}</p>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+              alt={item.name}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
